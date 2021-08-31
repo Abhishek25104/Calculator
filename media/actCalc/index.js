432a0abd08d5498cb051ce199acd7081911ba984
Abhishek Srivastava <secretagentabs11@gmail.com>
2021-08-31 15:15:07 +0530

    added calculator

--- media/actCalc/index.js
+++ media/actCalc/index.js
@@ -12,6 +12,13 @@ var calculate = document.querySelector(".calculate");
 
 
 
+var principle2 = document.querySelector("#principle2");
+var rate2= document.querySelector("#rate2");
+var time2 = document.querySelector("#time2");
+var frequency2 = document.querySelector("#frequency2");
+var calculate2 = document.querySelector(".calculate2");
+var output2 = document.querySelector("#output2");
+
 
 console.log(principle);
 console.log(rate);
@@ -19,12 +26,45 @@ console.log(time);
 console.log(calculate);
 console.log(output);
 
+
+console.log(principle2);
+console.log(rate2);
+console.log(time2);
+console.log(frequency2);
+console.log(calculate2);
+console.log(output2);
+
+
+
+
+
+
+
+
+
 calculate.addEventListener('click',function calculate(){
     var SI = (principle.value*rate.value*time.value)/100;
     console.log(principle.value);
     console.log(SI);
     output.innerHTML = "Simple Interest is: " + SI;
 });
+
+
+
+
+calculate2.addEventListener('click', function calculate(){
+   var Amt = principle2.value*(Math.pow((1+(rate2.value/frequency2.value)), (frequency2.value*time2.value)));
+    var CI = (Amt-principle2.value);
+    console.log(CI);
+    // console.log(output2);
+    document.getElementById("output2").innerHTML = "Compound Interest is: " + CI;
+});
+
+
+
+
+
+
 
 // function calculate(){
 //     var SI = (principle.value*rate.value*time.value)/100;
@@ -35,5 +75,4 @@ calculate.addEventListener('click',function calculate(){
 
 
 
-// var output2 = document.querySelector("#output2");
-// console.log(output2);
+
